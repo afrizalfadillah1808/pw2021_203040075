dd5cb4f27e4d7aee2d90ce4e5485293ae97c208e
afrizalfadillah1808 <muhammadafrizalfadillah@gmail.com>
2021-02-10 16:20:32 +0700

    menyelesaikan video pertemuan 2

--- pertemuan 2/index.php
+++ pertemuan 2/index.php
@@ -3,7 +3,7 @@
 Muhammad Afrizal Fadillah
 203040075
 https://github.com/afrizalfadillah1808/pw2021_203040075.git
-Pertemuan 2 - 11 Februari 2021
+Pertemuan 2 - 9 Februari 2021
 Mempelajari mengenai sintaks PHP
 */ 
 ?>
