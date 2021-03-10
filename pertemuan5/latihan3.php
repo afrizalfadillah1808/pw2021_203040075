c1829015c05d15ff84695a53563cdada05fe2d4f
Muhammad Afrizal Fadillah <muhammadafrizalfadillah@gmail.com>
2021-03-10 14:09:25 +0700

    menyelesaikan tugas pertemuan 5

---
+++ pertemuan5/latihan3.php
@@ -0,0 +1,25 @@
+<?php
+$mahasiswa = [
+["Hervin Fakhrul", "203040097", "Teknik Informatika", "hervinfakhrulm@gmail.com"],
+["Muhammad Rizky", "203040043", "Teknik Informatika", "muhammadrizky@gmail.com"],
+["Muhammad Afrizal Fadillah", "203040075", "Teknik Informatika", "mafrizalfadillah@gmail.com"]
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Daftar Mahasiswa</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <?php foreach( $mahasiswa as $mhs ) : ?>
+    <ul>
+        <li>Nama : <?= $mhs[0]; ?></li>
+        <li>NRP : <?= $mhs[1]; ?></li>
+        <li>Jurusan : <?= $mhs[2]; ?></li>
+        <li>Email : <?= $mhs[3]; ?></li>
+    </ul>
+    <?php endforeach; ?>
+</body>
+</html>
