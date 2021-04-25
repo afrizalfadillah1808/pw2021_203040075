80f74495ebb7a2856c909d438b1bd22bc654c685
afrizalfadillah1808 <75975641+afrizalfadillah1808@users.noreply.github.com>
2021-04-25 13:58:49 +0700

    Add files via upload

---
+++ kuliah/pertemuan7/latihan4.php
@@ -0,0 +1,16 @@
+<?php 
+    
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>POST</title>
+</head>
+<body>
+
+<h1>Selamat Datang, <?= $_POST["nama"]; ?>!</h1>
+
+</body>
+</html>
+© 2021 GitHub, Inc.
